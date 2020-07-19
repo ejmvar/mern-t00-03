87654457f2e2c19435dbe6ee1991bbffae92fc3a
Eduardo Martinez <ejmvar@gmail.com>
2020-07-19 01:01:50 -0300

    FeriadosList: connect (react-redux)

--- src/components/FeriadosList.js
+++ src/components/FeriadosList.js
@@ -1,15 +1,17 @@
 
 
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 
 
 
 const FeriadosList = (props) => {
     return (
         <div>
-            form FeriadosList props: 
+            form FeriadosList props:
         </div>
     )
 }
 
-export default FeriadosList;
+// export default FeriadosList;
+export default connect()(FeriadosList);
