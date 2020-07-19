1e6b694cb284a5da76248ea3b3413a1d02a119af
Eduardo Martinez <ejmvar@gmail.com>
2020-07-19 00:53:14 -0300

    FeriadoActions.js: Create Actions const { default: thunk } = require("redux-thunk"); // NOTE: async support FeriadoReducers.js : add code for every action api.js : requests (export methods) FeriadosActions: imports api FeriadosActions: List -> dispatch -> api.then(...) FeriadoComponent : call dispatch reducers/index.js: act as barrel for combined reducers (and default imported from folder) import combined reducers from store
    App.js: Provider with store
    StateFunctionalComponent

--- src/actions/store.js
+++ src/actions/store.js
@@ -1,9 +1,10 @@
 const { createStore, applyMiddleware, compose } = require("redux");
 const { default: thunk } = require("redux-thunk"); // NOTE: async support
 
+import { reducers } from "../reducers"; // default from "index.js"
 
 export const store = createStore( // createStore(reducer: Reducer<any, Action<any>>, 
-    {}, // STUB: add reducer
+    reducers, // {}, // STUB: add reducer
     compose(applyMiddleware(thunk))
 )
 
---
+++ src/reducers/index.js
@@ -0,0 +1,7 @@
+// import { combineReducers } from "redux";
+const { combineReducers } = require("redux");
+import { FeriadoReducer } from "./FeriadoReducer";
+
+export const reducers = combineReducers({
+    FeriadoReducer
+})
