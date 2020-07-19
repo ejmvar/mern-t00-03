7250ef82d2305eb438319b81e833bc7580c695c2
Eduardo Martinez <ejmvar@gmail.com>
2020-07-19 01:22:32 -0300

    FeriadosList: connect (react-redux) mapStateToTProps : // list is stored at reducer FeriadoReducer
    // NOTE: now i can access:
    // NOTE: this.props.feriadosList
    // NOTE: map actions to props

--- src/components/FeriadosList.js
+++ src/components/FeriadosList.js
@@ -2,7 +2,7 @@
 
 import React, { Component } from 'react'
 import { connect } from 'react-redux';
-
+import * as actions from "../actions/FeriadoActions";
 
 
 const FeriadosList = (props) => {
@@ -14,4 +14,28 @@ const FeriadosList = (props) => {
 }
 
 // export default FeriadosList;
-export default connect()(FeriadosList);
+
+// const mapStateToProps = state => {
+//     return {
+//         // list is stored at reducer FeriadoReducer
+//         // feriadosList: state.FeriadosList.list .feriadosList.list
+//         feriadosList: state.FeriadoReducer.list
+//     }
+// }
+// NOTE: simplified "mapStateToProps"
+const mapStateToProps = state => ({
+    // list is stored at reducer FeriadoReducer
+    // feriadosList: state.FeriadosList.list .feriadosList.list
+    feriadosList: state.FeriadoReducer.list
+})
+
+// NOTE: now i can access:
+// NOTE: this.props.feriadosList
+
+
+// NOTE: map actions to props
+const mapActionsToProps={
+    doListFeriados: actions.List
+}
+
+export default connect(mapStateToProps)(FeriadosList);
