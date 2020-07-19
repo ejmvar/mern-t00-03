824546860dea33e72e3e88ba1dc8a2e5b00cc717
Eduardo Martinez <ejmvar@gmail.com>
2020-07-18 23:14:44 -0300

    [ok] Apps.js

--- src/App.js
+++ src/App.js
@@ -1,32 +1,37 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { store } from './actions/store';
-import { Provider } from 'react-redux';
+import { FeriadoForm } from './components/FeriadoForm';
+import FeriadosList from './components/FeriadosList';
+// import { store } from './actions/store';
+// import { Provider } from 'react-redux';
+// <Provider store={store}>
+// </Provider>
 
-Provider
 function App() {
   return (
-    <Provider store={store}>
 
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
+    <div className="App">
+      <header className="App-header">
+        
+<FeriadoForm></FeriadoForm>
+<FeriadosList></FeriadosList>
+
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
         </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
         </a>
-        </header>
-      </div>
+      </header>
+    </div>
 
-    </Provider>
 
   );
 }
